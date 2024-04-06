c4c6150bcd0e8a9ee2ad8741afa34506d7a1c50c
ervanherdiansyah <77282241+ervanherdiansyah@users.noreply.github.com>
2024-04-06 13:54:32 +0700

    update

--- resources/views/dashboard/component/sidebar/sidebar.blade.php
+++ resources/views/dashboard/component/sidebar/sidebar.blade.php
@@ -3,7 +3,7 @@
     <div class="sidenav-header">
         <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
             aria-hidden="true" id="iconSidenav"></i>
-        @if (isset(Auth::user()->foto) != null)
+        {{-- @if (isset(Auth::user()->foto) != null)
             <a class="navbar-brand m-0" href=" {{ url('/dashboard/home') }}">
                 <img src="{{ asset('storage/' . Auth::user()->foto) }}" class="navbar-brand-img h-100" alt="main_logo">
                 <span class="ms-1 font-weight-bold">{{ Auth::user()->name }}</span>
@@ -13,7 +13,7 @@
                 <img src="{{ asset('argon') }}/assets/img/foto.png" class="navbar-brand-img h-100" alt="main_logo">
                 <span class="ms-1 font-weight-bold">{{ Auth::user()->name }}</span>
             </a>
-        @endif
+        @endif --}}
     </div>
     <hr class="horizontal dark mt-0">
     <div class="collapse navbar-collapse  w-auto " id="sidenav-collapse-main">
@@ -132,7 +132,7 @@
     </div>
     <div class="sidenav-footer mx-3 ">
         <div class="card card-plain shadow-none" id="sidenavCard">
-            <img class="w-50 mx-auto" src="{{ asset('assets/img/logo-fojb.png') }}" alt="sidebar_illustration">
+            {{-- <img class="w-50 mx-auto" src="{{ asset('assets/img/logo-fojb.png') }}" alt="sidebar_illustration"> --}}
             <div class="card-body text-center p-3 w-100 pt-0">
                 <div class="docs-info">
                     <h6 class="mb-0">WEBSITE OPREC ANGGOTA</h6>
